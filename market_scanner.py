93270ef50d5902e2c68f58ccb602b2b0e830a767
Alex Shirley <alex.shirley@egym.de>
2020-07-31 18:03:38 +0200

    Make Anomaly Finding more Pandas friendly
    This removes the unused finding function, and replaces it with the
    normal algorithm. Then, all the indexing through the Pandas Dataframes
    are refactored in preference for the faster Pandas calls.

--- market_scanner.py
+++ market_scanner.py
@@ -20,41 +20,21 @@ class mainObj:
         sys.stdout = sys.__stdout__
         return data[["Volume"]]
 
-    def find_anomalies(self, data, cutoff):
-        anomalies = []
-        data_std = np.std(data['Volume'])
-        data_mean = np.mean(data['Volume'])
-        anomaly_cut_off = data_std * cutoff
-        upper_limit = data_mean + anomaly_cut_off
-        indexs = data[data['Volume'] > upper_limit].index.tolist()
-        outliers = data[data['Volume'] > upper_limit].Volume.tolist()
-        index_clean = [str(x)[:-9] for x in indexs]
-        d = {'Dates': index_clean, 'Volume': outliers}
-        return d
 
-    def find_anomalies_two(self, data, cutoff):
-        indexs = []
-        outliers = []
+    def find_anomalies(self, data, cutoff, currentDate):
         data_std = np.std(data['Volume'])
         data_mean = np.mean(data['Volume'])
         anomaly_cut_off = data_std * cutoff
         upper_limit = data_mean + anomaly_cut_off
         data.reset_index(level=0, inplace=True)
-        for i in range(len(data)):
-            temp = data['Volume'].iloc[i]
-            if temp > upper_limit:
-                indexs.append(str(data['Date'].iloc[i])[:-9])
-                outliers.append(temp)
-        d = {'Dates': indexs, 'Volume': outliers}
-        return d
+        is_outlier = data['Volume'] > upper_limit
+        is_in_three_days = ((currentDate - data['Date']) <= datetime.timedelta(days=3))
+        return data[is_outlier & is_in_three_days]
 
     def customPrint(self, d, tick):
         print("\n\n\n*******  " + tick.upper() + "  *******")
         print("Ticker is: "+tick.upper())
-        for i in range(len(d['Dates'])):
-            str1 = str(d['Dates'][i])
-            str2 = str(d['Volume'][i])
-            print(str1 + " - " + str2)
+        print(d)
         print("*********************\n\n\n")
 
     def days_between(self, d1, d2):
@@ -68,12 +48,11 @@ class mainObj:
         currentDate = datetime.datetime.strptime(
             date.today().strftime("%Y-%m-%d"), "%Y-%m-%d")
         start_time = time.time()
-        for x in tqdm(list_of_tickers):
-            d = (self.find_anomalies_two(self.getData(x), cutoff))
-            if d['Dates']:
-                for i in range(len(d['Dates'])):
-                    if self.days_between(str(currentDate)[:-9], str(d['Dates'][i])) <= 3:
-                        self.customPrint(d, x)
+        for x in tqdm(list_of_tickers, miniters=1):
+            d = (self.find_anomalies(self.getData(x), cutoff, currentDate))
+            if d.empty:
+                continue
+            self.customPrint(d, x)
 
         print("\n\n\n\n--- this took %s seconds to run ---" %
               (time.time() - start_time))
